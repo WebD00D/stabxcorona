7dde79c1ecf28f9ca6d034cb9646221178fecaad
WebD00D <christian@stabmag.com>
2019-12-15 13:37:09 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import {
+  SplitPane,
+  Pane,
+  StabLogo,
+  CoronaLogo,
+  Content,
+  Button,
+  Form,
+  FormLabel,
+  Error
+} from "./styles";
+import Stab from "./stab-logo@2x.png";
+import Corona from "./corona.png";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const maxLengthError = () => {
+    const total = message.split(" ");
+    if (total.length > 25) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const checkDisabled = () => {
+    let disabled = false;
+    if (email.trim() === "" || message.trim() === "" || maxLengthError()) {
+      disabled = true;
+    }
+    return disabled;
+  };
+
+  const renderError = () => {
+    const total = message.split(" ").length;
+    const cx = total > 25 ? "error" : "";
+    return <Error className={cx}>No more than 25 words, please :)</Error>;
+  };
+
+  return (
+    <SplitPane>
+      {renderError()}
+      <Pane>
+        <StabLogo src={Stab} />
+        <Content>
+          <h1>
+            Win The Electric Acid Surfboard Test Boards Piloted by Stephanie
+            Gilmore.
+          </h1>
+          <p>
+            The Electric Acid Surfboard Test is an exercise in diversity. We
+            live in accepting times where no craft is a faux pas, so we decided
+            to celebrate by pairing the world’s best surfers with every manner
+            of surf craft imaginable. The second Electric Acid Surfboard Test
+            was conducted by 7X World Champion Miss Stephanie Gilmore, and now,
+            thanks to our amigos at Corona, the boards are yours to win. Plug
+            your details in below for your chance to win a board crafted by
+            Shawn Stüssy, MR, Joel Fitzgerald and more.
+          </p>
+          <Form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contest" />
+            <FormLabel>Email Address</FormLabel>
+            <input
+              name="email"
+              type="email"
+              onChange={e => setEmail(e.currentTarget.value)}
+            />
+            <FormLabel>
+              In 25 words or less, describe the surfing style of Miss Stephanie
+              Gilmore:
+            </FormLabel>
+            <textarea
+              name="message"
+              type="text"
+              onChange={e => setMessage(e.currentTarget.value)}
+            />
+            <Button disabled={checkDisabled()}>Submit</Button>
+          </Form>
+        </Content>
+      </Pane>
+      <Pane showBG={true}>
+        <CoronaLogo src={Corona} />
+      </Pane>
+    </SplitPane>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,189 @@
+import styled from "styled-components";
+import RightSideImage from "./right-side-image2.jpg";
+
+export const SplitPane = styled.div`
+  display: flex;
+  height: calc(100vh - 64px);
+
+  @media (max-width: 972px) {
+    flex-direction: column-reverse;
+    height: auto;
+  }
+`;
+
+export const Pane = styled.div`
+  padding: 32px 64px;
+  flex: 1;
+  height: 100%;
+  background-color: #fafafa;
+  overflow-y: scroll;
+
+  ${props =>
+    props.showBG
+      ? `background: linear-gradient( rgba(01,01,01,0.3), rgba(01,01,01,0.3) ), url(${RightSideImage});`
+      : null}
+
+  background-position: center;
+  background-size: cover;
+  background-repeat: center;
+
+  @media (max-width: 972px) {
+
+    padding: 32px;
+
+      
+    ${props =>
+      props.showBG
+        ? `min-height: 320px; background-position: top; background-attachment: fixed;`
+        : null}
+  }
+`;
+
+export const StabLogo = styled.img`
+  height: 32px;
+`;
+
+export const CoronaLogo = styled.img`
+  position: absolute;
+  bottom: 32px;
+  right: 64px;
+  height: 64px;
+
+  @media (max-width: 972px) {
+      top: 16px;
+      right: 16px;
+      height: 32px;
+  }
+`;
+
+export const Content = styled.div`
+  padding-top: 32px;
+
+  h1 {
+    font-family: "Roboto", sans-serif;
+  }
+
+  p {
+    margin-bottom: 32px;
+    line-height: 1.5;
+    font-family: "PT Serif", serif;
+  }
+
+  input,
+  textarea {
+    font-family: "Roboto", sans-serif;
+    font-size: 16px;
+    padding-left: 16px;
+    padding-right: 16px;
+    border: none;
+    outline: none;
+    border-radius: 4px;
+    max-width: 600px;
+    width: 100%;
+    -webkit-box-shadow: 3px 2px 15px -3px rgba(0, 0, 0, 0.2);
+    -moz-box-shadow: 3px 2px 15px -3px rgba(0, 0, 0, 0.2);
+    box-shadow: 3px 2px 15px -3px rgba(0, 0, 0, 0.2);
+    transition: 0.2s ease;
+    border: 2px solid #fff;
+  }
+
+  input:focus,
+  input:active,
+  textarea:focus,
+  textarea:active {
+    border: 2px solid #ccc;
+  }
+
+  input {
+    margin-bottom: 32px;
+    height: 48px;
+  }
+
+  textarea {
+    margin-bottom: 32px;
+    padding: 16px;
+    resize: none;
+    height: 150px;
+  }
+
+  @media (max-width: 972px) {
+      padding-top: 12px;
+
+      input, textarea {
+          width: calc(100% - 32px);
+      }
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FormLabel = styled.div`
+  font-family: "Roboto", sans-serif;
+  margin-bottom: 8px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  text-transform: uppercase;
+  line-height: 1.5;
+`;
+
+export const Button = styled.button`
+  border-radius: 50px;
+  height: 48px;
+  background-color: #000;
+  outline: none;
+  color: #fff;
+  font-family: "Roboto", sans-serif;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  text-transform: uppercase;
+  width: 250px;
+  cursor: pointer;
+  transition: 0.2s ease;
+  line-height: 48px;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+
+    &:hover {
+      transform: scale(1);
+    }
+  }
+`;
+
+export const Error = styled.div`
+  position: fixed;
+  opacity: 0;
+  left: 0;
+  width: 100%;
+  height: 32px;
+  background-color: #d53b49;
+  color: #fff;
+  font-family: "Roboto", sans-serif;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  -webkit-box-shadow: 3px 2px 15px -3px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 3px 2px 15px -3px rgba(0, 0, 0, 0.2);
+  box-shadow: 3px 2px 15px -3px rgba(0, 0, 0, 0.2);
+  transition: 0.2s ease;
+  top: 0px;
+  z-index: 2;
+
+  &.error {
+    opacity: 1;
+  }
+`;
